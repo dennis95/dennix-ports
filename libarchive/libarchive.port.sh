71973ecbd912936a90baa61bf8d4decd561108bb
Dennis Wölfing <denniswoelfing@gmx.de>
2020-04-29 15:23:29 +0200

    Add libarchive port.
    Note that the port currently cannot extract archives because we don't
    have a chown function yet.

---
+++ libarchive/libarchive.port.sh
@@ -0,0 +1,17 @@
+depends=libz
+version=3.4.2
+src_url=https://www.libarchive.org/downloads/libarchive-$version.tar.xz
+src_type=tar.xz
+src_sha256=d8e10494b4d3a15ae9d67a130d3ab869200cfd60b2ab533b391b0a0d5500ada1
+patches=0001-libarchive.patch
+buildsystem=configure
+extra_configure_flags=--disable-maintainer-mode
+install_target=install-strip
+license_files=COPYING
+purge=lib/libarchive.la
+
+install_port() {
+    make $install_targets DESTDIR="$SYSROOT"
+    ln -f "$SYSROOT/bin/bsdcpio" "$SYSROOT/bin/cpio"
+    ln -f "$SYSROOT/bin/bsdtar" "$SYSROOT/bin/tar"
+}
